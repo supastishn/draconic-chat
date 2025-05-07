f498dcf8340ed6ecc31f50a2737bdb52222bfcef
supastishn <omareeto2012@hotmail.com>
2025-05-07 14:04:56 +0000

    chore: Set Appwrite endpoint URL

--- src/appwrite.js
+++ src/appwrite.js
@@ -3,7 +3,7 @@ import { Client, Databases, ID, Query } from 'appwrite';
 const client = new Client();
 
 client
-    .setEndpoint('YOUR_APPWRITE_ENDPOINT') // Replace with your Appwrite server endpoint
+    .setEndpoint('https://fra.cloud.appwrite.io/v1') // Replace with your Appwrite server endpoint
     .setProject('draconic-chatroom');     // Project ID from your appwrite.json or Appwrite console
 
 const databases = new Databases(client);
